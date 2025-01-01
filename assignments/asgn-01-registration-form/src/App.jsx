c9b05ffcf351e240869404ec67aa9d38e0fe8c32
Ken <ken.gy.leung@gmail.com>
2025-01-01 21:49:41 +1100

    Added flex layout for form and radio group specifically after changing the html structure for it

--- assignments/asgn-01-registration-form/src/App.jsx
+++ assignments/asgn-01-registration-form/src/App.jsx
@@ -26,8 +26,14 @@ function App() {
         <label htmlFor="date-of-birth">Date of Birth</label>
         <input type="date" id="date-of-birth" />
         <label htmlFor="gender">Gender</label>
-        <input type="radio" name="gender" value="male" /> Male
-        <input type="radio" name="gender" value="female" /> Female
+        <div className="radio-group">
+          <div>
+            <input type="radio" name="gender" value="male" /> Male
+          </div>
+          <div>
+            <input type="radio" name="gender" value="female" /> Female
+          </div>
+        </div>
         <label htmlFor="about-me">About Me</label>
         <textarea id="about-me" placeholder="About Me"></textarea>
         <label htmlFor="profession">Choose Your Profession</label>
