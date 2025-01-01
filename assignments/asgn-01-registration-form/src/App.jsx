727dd6f55f04db1fddf7e167b7471bfb6c0352b7
Ken <ken.gy.leung@gmail.com>
2025-01-01 21:20:01 +1100

    Add form HTML tags like label, input and button with basic attributes

--- assignments/asgn-01-registration-form/src/App.jsx
+++ assignments/asgn-01-registration-form/src/App.jsx
@@ -8,28 +8,39 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form>
+        <label htmlFor="first-name">First Name</label>
+        <input type="text" id="first-name" placeholder="First Name" />
+        <label htmlFor="last-name">Last Name</label>
+        <input type="text" id="last-name" placeholder="Last Name" />
+        <label htmlFor="email-address">Email Address</label>
+        <input type="email" id="email-address" placeholder="Email Address" />
+        <label htmlFor="password">Password</label>
+        <input type="password" id="password" placeholder="Password" />
+        <label htmlFor="confirm-password">Confirm Password</label>
+        <input
+          type="password"
+          id="confirm-password"
+          placeholder="Confirm Password"
+        />
+        <label htmlFor="date-of-birth">Date of Birth</label>
+        <input type="date" id="date-of-birth" />
+        <label htmlFor="gender">Gender</label>
+        <input type="radio" name="gender" value="male" /> Male
+        <input type="radio" name="gender" value="female" /> Female
+        <label htmlFor="about-me">About Me</label>
+        <textarea id="about-me" placeholder="About Me"></textarea>
+        <label htmlFor="profession">Choose Your Profession</label>
+        <select id="profession">
+          <option value="web-development">Web Development</option>
+          <option value="data-analytics">Data Analytics</option>
+          <option value="ui-ux-design">UI/UX Design</option>
+          <option value="others">Others</option>
+        </select>
+        <button type="submit">Submit</button>
+      </form>
     </>
-  )
+  );
 }
 
 export default App
