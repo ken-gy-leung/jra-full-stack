cbd0fd7e4ee1b39fb59181f1b4c8a820cefa213e
ken-gy-leung <ken.gy.leung@gmail.com>
2025-01-14 19:29:26 +1100

    Deleted unnecessary imports by vite template

--- assignments/asgn-02-card-ui-design/src/App.jsx
+++ assignments/asgn-02-card-ui-design/src/App.jsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 import DemoCard1 from "./components/DemoCard1";
 
 function App() {
@@ -37,4 +34,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
