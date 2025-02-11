759a66667b4e6662c28bedd28f15b23dc0883dda
ken-gy-leung <ken.gy.leung@gmail.com>
2025-02-12 00:53:55 +1100

    Added all-done-undone filter for tasks.

--- assignments/asgn-05-react-todo-list-app/src/App.js
+++ assignments/asgn-05-react-todo-list-app/src/App.js
@@ -19,38 +19,45 @@ const App = () => {
   })
 
   const [taskList, setTaskList] = useState(taskDataWithId)
+  const [filter, setFilter] = useState('all')
+  
+  const handleFilterChange = event => {
+    setFilter(event.target.value)
+  }
 
   const handleToggle = (id, done) => {
     const updatedTasks = taskList.map(task=>task.id===id? {...task,done}:task)
     setTaskList(updatedTasks)
-    console.log(updatedTasks)
   }
 
-  const handleAdd = (content)=>{
-    console.log(taskList)
+  const handleAdd = content => {
     const newTask = { id: v4(), content, done: false }
     setTaskList([...taskList, newTask])
-    console.log(taskList)
   }
 
-  const tasks = taskList.map(task => {
-    return (
-      <Task
-        key={task.id}
-        content={task.content}
-        done={task.done}
-        onToggle={done => handleToggle(task.id, done)}
-      />
-    )
-  })
+  const allTasks = taskList.map(task => 
+    <Task
+      key={task.id}
+      content={task.content}
+      done={task.done}
+      onToggle={done => handleToggle(task.id, done)}
+    />
+  )
+
+  const filteredTasks = filter !== 'all' ? allTasks.filter(task => task.props.done === !!Number(filter)) : allTasks
 
   return (
     <div className="App">
-      <div className='task-adding'>
+      <div className="task-adding">
         <TaskAddingWidget onAdd={handleAdd} />
       </div>
-      <div className='task-list'>
-        {tasks}
+      <select className='task-filter' onChange={handleFilterChange}>
+        <option value="all">All</option>
+        <option value="1">Done</option>
+        <option value="0">Undone</option>
+      </select>
+      <div className="task-list">
+        {filteredTasks}
       </div>
     </div>
   )
