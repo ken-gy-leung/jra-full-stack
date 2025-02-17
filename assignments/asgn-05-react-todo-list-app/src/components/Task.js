bc516080a5cc5f68d3716282802cbac2a83ecbe0
ken-gy-leung <ken.gy.leung@gmail.com>
2025-02-17 12:40:09 +1100

    Optimaized the layout of Task title textarea with downloaded fonts and height auto resizing

--- assignments/asgn-05-react-todo-list-app/src/components/Task.js
+++ assignments/asgn-05-react-todo-list-app/src/components/Task.js
@@ -1,4 +1,5 @@
-import { getCurrentDateTime } from '../utils/utils'
+// import { getCurrentDateTime } from '../utils/utils'
+import { useEffect, useRef } from 'react'
 
 const Task = ({ status, deadline, title, content, done, onDoneToggle, onDeadlineChange, onTitleChange, onContentChange, onTaskDelete }) => {
     const toggleDone = event => {
@@ -17,32 +18,32 @@ const Task = ({ status, deadline, title, content, done, onDoneToggle, onDeadline
         onContentChange(event.target.value)
     }
 
+    const titleRef = useRef(null)
+
+    const pxToRem = px => {
+        const baseFontSize = parseFloat(getComputedStyle(document.documentElement).fontSize)
+        return px / baseFontSize
+    }
+
+    const adjustHeight = element => {
+        element.style.height = 'auto'
+        console.log('title input:')
+        console.log(element)
+        let heightInRem = pxToRem(element.scrollHeight)
+        console.log('scroll height:', `${heightInRem}rem`)
+        heightInRem = heightInRem < 4.7 ? heightInRem - 2 : heightInRem - 1
+        element.style.height = `${heightInRem}rem`
+    }
+
+    useEffect(() => {
+        // if (titleRef.current) {
+            adjustHeight(titleRef.current)
+        // }
+    }, [title])
+
     return (
         <div className='task'>
-            <input
-                className={`task-date-time-picker task-${status}`}
-                type='datetime-local'
-                // use current date & time as min value for datetime-local input
-                // min={getCurrentDateTime()}
-                value={deadline}
-                onChange={changeDeadline}
-            />
-            <input
-                className={`task-title task-${status}`}
-                type='text'
-                name='title-input'
-                value={title}
-                onChange={changeTitle}
-            />
-            <input
-                className={`task-content task-${status}`}
-                type='text'
-                name='content-input'
-                value={content}
-                onChange={changeContent}
-            />
             <div className='task-controls'>
-                <div className='task-delete-button' onClick={onTaskDelete}>&#10060;</div>
                 <input
                     className='task-checkbox'
                     type='checkbox'
@@ -50,7 +51,31 @@ const Task = ({ status, deadline, title, content, done, onDoneToggle, onDeadline
                     checked={done}
                     onChange={toggleDone}
                 />
+                <input
+                className={`task-date-time-picker task-${status}`}
+                type='datetime-local'
+                // use current date & time as min value for datetime-local input
+                // min={getCurrentDateTime()}
+                value={deadline}
+                onChange={changeDeadline}
+            />
+                <div className='task-delete-button' onClick={onTaskDelete}>&#10060;</div>
             </div>
+            <textarea
+                ref={titleRef}
+                className={`task-title task-${status}`}
+                type='text'
+                name='title-input'
+                value={title}
+                onChange={changeTitle}
+            />
+            <textarea
+                className={`task-content task-${status}`}
+                type='text'
+                name='content-input'
+                value={content}
+                onChange={changeContent}
+            />
         </div>
     )
 }
